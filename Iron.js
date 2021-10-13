0d596a8c1144a1d7edc2c7913965964cd1d6f4a9
Luis13625283 <90009089+Luis13625283@users.noreply.github.com>
2021-10-13 15:19:32 -0500

    Add files via upload

---
+++ Iron.js
@@ -0,0 +1,9 @@
+class Iron{
+    constructor(x, y){
+        var options={
+        'density':30,
+        'friction': 3,
+        'restitution':0.8
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Geologo
+# Geologist
+Geologist Project
